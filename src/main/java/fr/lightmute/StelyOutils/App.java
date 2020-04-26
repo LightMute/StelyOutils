5c92b9859a4f0a834ee04d9d43d132d04208ed47
LightMute <admin@admin.fr>
2020-04-26 13:26:49 +0200

    firt save

---
+++ src/main/java/fr/lightmute/StelyOutils/App.java
@@ -0,0 +1,41 @@
+package fr.lightmute.StelyOutils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.event.EventHandler;
+
+public class App extends JavaPlugin implements Listener{
+	
+	String prefix = "§7[§cStelyWarning§7] §c";
+	
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+	public void onDisable() {
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void getoutilsdurab(PlayerInteractEvent e) {
+		Player player = e.getPlayer();
+		
+		if(player.getItemInHand() != null) {
+			if(player.getItemInHand().getDurability() / 2 == player.getItemInHand().getDurability()) {
+				player.sendMessage(prefix + player.getItemInHand().getI18NDisplayName() + " devient faible !");
+				player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
+			}else if(player.getItemInHand().getDurability() / 3 == player.getItemInHand().getDurability()) {
+				player.sendMessage(prefix + player.getItemInHand().getI18NDisplayName() + " devient tres faible !");
+				player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
+			}else if(player.getItemInHand().getDurability() / 4 == player.getItemInHand().getDurability()) {
+				player.sendMessage(prefix + player.getItemInHand().getI18NDisplayName() + " va cassé !");
+				player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
+			}
+		}
+	}
+}
