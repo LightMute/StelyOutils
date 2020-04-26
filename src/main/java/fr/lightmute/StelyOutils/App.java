a7575d5788efe7640617870f48570b825da85f96
LightMute <admin@admin.fr>
2020-04-26 14:52:20 +0200

    Update

--- src/main/java/fr/lightmute/StelyOutils/App.java
+++ src/main/java/fr/lightmute/StelyOutils/App.java
@@ -3,12 +3,11 @@ package fr.lightmute.StelyOutils;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.md_5.bungee.event.EventHandler;
 
 public class App extends JavaPlugin implements Listener{
 
@@ -25,18 +24,16 @@ public class App extends JavaPlugin implements Listener{
 	@EventHandler
 	public void getoutilsdurab(PlayerInteractEvent e) {
 		Player player = e.getPlayer();
-		if(e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.LEFT_CLICK_BLOCK) {
-			Bukkit.broadcastMessage("ok");
-			if(player.getItemInHand().getType().getMaxDurability()/ 2 <= player.getItemInHand().getDurability() && player.getItemInHand().getType().getMaxDurability() / 3 > player.getItemInHand().getDurability()) {
-				player.sendMessage(prefix + player.getItemInHand().getItemMeta().getDisplayName() + " devient faible !");
-				player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
-			}else if(player.getItemInHand().getType().getMaxDurability()/ 3 <= player.getItemInHand().getDurability() && player.getItemInHand().getType().getMaxDurability() / 4 > player.getItemInHand().getDurability()) {
-				player.sendMessage(prefix + player.getItemInHand().getItemMeta().getDisplayName() + " devient trés faible !");
-				player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
-			}else if(player.getItemInHand().getType().getMaxDurability() / 4 <= player.getItemInHand().getDurability()) {
-				player.sendMessage(prefix + player.getItemInHand().getItemMeta().getDisplayName() + " va cassé !");
-				player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
-			}
+
+		if(player.getItemInHand().getType().getMaxDurability()/ 2 <= player.getItemInHand().getDurability() && player.getItemInHand().getType().getMaxDurability() / 3 > player.getItemInHand().getDurability()) {
+			player.sendMessage(prefix + player.getItemInHand().getItemMeta().getDisplayName() + " devient faible !");
+			player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
+		}else if(player.getItemInHand().getType().getMaxDurability()/ 3 <= player.getItemInHand().getDurability() && player.getItemInHand().getType().getMaxDurability() / 4 > player.getItemInHand().getDurability()) {
+			player.sendMessage(prefix + player.getItemInHand().getItemMeta().getDisplayName() + " devient trés faible !");
+			player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
+		}else if(player.getItemInHand().getType().getMaxDurability() / 4 <= player.getItemInHand().getDurability()) {
+			player.sendMessage(prefix + player.getItemInHand().getItemMeta().getDisplayName() + " va cassé !");
+			player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
 		}
 	}
 }
