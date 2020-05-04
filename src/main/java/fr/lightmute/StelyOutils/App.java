1da789eab31a9ea00c76b7acbedd66fd9663b33b
LightMute <admin@admin.fr>
2020-05-04 09:37:54 +0200

    Update spam message

--- src/main/java/fr/lightmute/StelyOutils/App.java
+++ src/main/java/fr/lightmute/StelyOutils/App.java
@@ -1,5 +1,7 @@
 package fr.lightmute.StelyOutils;
 
+import java.util.HashMap;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -11,6 +13,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class App extends JavaPlugin implements Listener{
 
 	String prefix = "§7[§cStelyWarning§7] §c";
+	HashMap<String, Boolean> playerspam = new HashMap<String, Boolean>();
 
 	public void onEnable() {
 		Bukkit.getPluginManager().registerEvents(this, this);
@@ -24,9 +27,20 @@ public class App extends JavaPlugin implements Listener{
 	public void getoutilsdurab(PlayerInteractEvent e) {
 		Player player = e.getPlayer();
 
-		if((Integer.valueOf(player.getItemInHand().getType().getMaxDurability())/ 3) <= player.getItemInHand().getDurability()) {
-			player.sendActionBar(prefix + player.getItemInHand().getItemMeta().getDisplayName() + " devient faible !");
-			player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
+		if(!playerspam.containsKey(player.getName())) {
+			playerspam.put(player.getName(), true);
+		}
+
+		if(Integer.valueOf(player.getItemInHand().getDurability()) <= 0) {
+			playerspam.replace(player.getName(), true);
+		}
+		
+		if((Integer.valueOf(player.getItemInHand().getType().getMaxDurability())/ 4) < Integer.valueOf(player.getItemInHand().getDurability()) && (Integer.valueOf(player.getItemInHand().getType().getMaxDurability())/ 5) > Integer.valueOf(player.getItemInHand().getDurability())) {
+			if(playerspam.get(player.getName()).equals(true)){
+				player.sendActionBar(prefix + player.getItemInHand().getItemMeta().getDisplayName() + " devient faible !");
+				player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 900.0F, 1.0F);
+				playerspam.replace(player.getName(), false);
+			}
 		}
 	}
 }
