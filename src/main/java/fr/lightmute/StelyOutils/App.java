833bb87d43062d3b668d9baa5b7a0d43ced7f9ea
LightMute <admin@admin.fr>
2020-05-14 20:05:16 +0200

    Update test 3

--- src/main/java/fr/lightmute/StelyOutils/App.java
+++ src/main/java/fr/lightmute/StelyOutils/App.java
@@ -30,10 +30,13 @@ public class App extends JavaPlugin implements Listener{
 		if(Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) < 10 && Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) > 5) {
 			player.sendActionBar(prefix + "Ton outils est trés faible !");
 			player.playSound(player.getLocation(), Sound.BLOCK_TRIPWIRE_CLICK_OFF, 900.0F, 1.0F);
-		}
-		if(Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) < 50 && Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) > 45) {
+		}else if(Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) < 25 && Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) > 20) {
 			player.sendActionBar(prefix + "Ton outils est faible !");
 			player.playSound(player.getLocation(), Sound.BLOCK_TRIPWIRE_CLICK_OFF, 900.0F, 1.0F);
+		}else {
+			player.sendActionBar(prefix + "§a" +Integer.valueOf(player.getItemInHand().getDurability()) + "§7/§c" + Integer.valueOf(player.getItemInHand().getType().getMaxDurability()));
 		}
+		
+		
 	}
 }
