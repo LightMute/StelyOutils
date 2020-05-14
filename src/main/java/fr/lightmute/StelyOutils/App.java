2ffcba78299de5e79d2ffa9ca7afab971ac417aa
LightMute <admin@admin.fr>
2020-05-14 21:29:34 +0200

    Update test 05

--- src/main/java/fr/lightmute/StelyOutils/App.java
+++ src/main/java/fr/lightmute/StelyOutils/App.java
@@ -12,7 +12,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class App extends JavaPlugin implements Listener{
 
-	String prefix = "§7[§cStelyOutils§7] §c";
+	String prefix = "§7[§5StelyOutils§7] §c";
 	HashMap<String, Boolean> playerspam = new HashMap<String, Boolean>();
 
 	public void onEnable() {
@@ -28,13 +28,13 @@ public class App extends JavaPlugin implements Listener{
 		Player player = e.getPlayer();
 
 		if(Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) < 10 && Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) > 5) {
-			player.sendActionBar(prefix + "Ton outils est trés faible !");
+			player.sendActionBar(prefix + "Ton outils va casser");
 			player.playSound(player.getLocation(), Sound.BLOCK_TRIPWIRE_CLICK_OFF, 900.0F, 1.0F);
 		}else if(Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) < 25 && Integer.valueOf(player.getItemInHand().getType().getMaxDurability()) - Integer.valueOf(player.getItemInHand().getDurability()) > 20) {
-			player.sendActionBar(prefix + "Ton outils est faible !");
+			player.sendActionBar(prefix + "Ton outils est abîmé");
 			player.playSound(player.getLocation(), Sound.BLOCK_TRIPWIRE_CLICK_OFF, 900.0F, 1.0F);
 		}else {
-			player.sendActionBar(prefix + "§a" +Integer.valueOf(player.getItemInHand().getDurability()) + "§7/§c" + Integer.valueOf(player.getItemInHand().getType().getMaxDurability()));
+			player.sendActionBar(prefix + "§a" + (Integer.valueOf(player.getItemInHand().getDurability()) - Integer.valueOf(player.getItemInHand().getType().getMaxDurability())) + "§7/§a" + Integer.valueOf(player.getItemInHand().getType().getMaxDurability()));
 		}
 		
 		
