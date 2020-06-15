1fecf1e067ec8c3b3bdcd7d5dc59a90e1e753bba
LightMute <admin@admin.fr>
2020-06-15 15:57:01 +0200

    Add methode getarmure

--- src/main/java/fr/lightmute/StelyOutils/App.java
+++ src/main/java/fr/lightmute/StelyOutils/App.java
@@ -1,13 +1,16 @@
 package fr.lightmute.StelyOutils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class App extends JavaPlugin implements Listener{
@@ -30,12 +33,44 @@ public class App extends JavaPlugin implements Listener{
 		if(player.getItemInHand() != null) {
 
 			if(Integer.valueOf(e.getItem().getType().getMaxDurability()) - Integer.valueOf(e.getItem().getDurability()) < 10 && Integer.valueOf(e.getItem().getType().getMaxDurability()) - Integer.valueOf(e.getItem().getDurability()) > 5) {
-				player.sendActionBar(prefix + "Ton outils "+ e.getItem().getType().toString() +" va casser !");
+				player.sendActionBar(prefix + "Ton outils "+ getarmure(e.getItem()) +" va casser !");
 				player.playSound(player.getLocation(), Sound.BLOCK_TRIPWIRE_CLICK_OFF, 900.0F, 1.0F);
 			}else if(Integer.valueOf(e.getItem().getType().getMaxDurability()) - Integer.valueOf(e.getItem().getDurability()) < 25 && Integer.valueOf(e.getItem().getType().getMaxDurability()) - Integer.valueOf(e.getItem().getDurability()) > 20) {
-				player.sendActionBar(prefix + "Ton outils "+ e.getItem().getType().toString() +" est abîmé");
+				player.sendActionBar(prefix + "Ton outils "+ getarmure(e.getItem()) +" est abîmé");
 				player.playSound(player.getLocation(), Sound.BLOCK_TRIPWIRE_CLICK_OFF, 900.0F, 1.0F);
 			}
+		}
+	}
+
+	ArrayList<String> armures = new ArrayList<String>();
+	public String getarmure(ItemStack item) {
+		for(Material armure : Material.values()) {
+			if(armure.toString().contains("_HELMET")) {
+				if(!armures.contains(armure.toString())) {
+					armures.add(armure.toString());
+				}
+			}
+			if(armure.toString().contains("_CHESTPLATE")) {
+				if(!armures.contains(armure.toString())) {
+					armures.add(armure.toString());
+				}
+			}
+			if(armure.toString().contains("_LEGGINGS")) {
+				if(!armures.contains(armure.toString())) {
+					armures.add(armure.toString());
+				}
+			}
+			if(armure.toString().contains("_BOOTS")) {
+				if(!armures.contains(armure.toString())) {
+					armures.add(armure.toString());
+				}
+			}
+		}
+
+		if(armures.contains(item.getType().toString())) {
+			return "armure";
+		}else {
+			return "outils";
 		}
 	}
 }
